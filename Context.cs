3a63364a38a06ab8b427410a022e0ae9d4197956
hnkatze <henriquezhector1@hotmail.com>
2023-05-20 14:37:34 -0600

    Se agrego primero en enlaces de docker

---
+++ Context.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+
+public class Context:DbContext {
+
+
+
+
+
+
+public Context(DbContextOptions<Context> options) : base(options){}
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = "Data Source=localhost,1433; Initial Catalog=LibreriaDB; user ID=SA; Password=C@m1l1t0*; TrustServerCertificate=True";
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
